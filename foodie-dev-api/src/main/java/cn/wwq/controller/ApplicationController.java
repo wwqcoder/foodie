4df2418c693ebc7da2cd8a40e6a1406a15becf68
wangweiqi <799819401@qq.com>
2019-11-09 20:39:53 +0800

    天天吃货项目初始化提交！

---
+++ foodie-dev-api/src/main/java/cn/wwq/controller/ApplicationController.java
@@ -0,0 +1,13 @@
+package cn.wwq.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
