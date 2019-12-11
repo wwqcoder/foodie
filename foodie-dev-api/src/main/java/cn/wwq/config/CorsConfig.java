9a1b3e401d5096aec6fc8f088619cbe164c0fdd5
wangweiqi <799819401@qq.com>
2019-12-11 17:47:01 +0800

    修改配置

--- foodie-dev-api/src/main/java/cn/wwq/config/CorsConfig.java
+++ foodie-dev-api/src/main/java/cn/wwq/config/CorsConfig.java
@@ -18,6 +18,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:8080");
         config.addAllowedOrigin("http://180.76.235.76:8080");
+        config.addAllowedOrigin("*");
 
         //设置是否发送cookie信息
         config.setAllowCredentials(true);
