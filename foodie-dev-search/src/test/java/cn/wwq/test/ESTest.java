a2bf54a5ff46b1c3795314d69adb0acd370fd6a4
wangweiqi <799819401@qq.com>
2020-01-14 23:07:54 +0800

    ES 高亮显示及排序

--- foodie-dev-search/src/test/java/cn/wwq/test/ESTest.java
+++ foodie-dev-search/src/test/java/cn/wwq/test/ESTest.java
@@ -3,8 +3,16 @@ package cn.wwq.test;
 import cn.wwq.Application;
 import cn.wwq.pojo.Stu;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +20,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -97,6 +110,69 @@ public class ESTest {
                 .build();
 
         AggregatedPage<Stu> stus = esTemplate.queryForPage(searchQuery, Stu.class);
+
+        System.out.println("总分页数目"+stus.getTotalPages());
+        for (Stu stu : stus.getContent()) {
+            System.out.println(stu);
+        }
+    }
+
+    @Test
+    public void hignlightStuDoc(){
+
+        String preTag = "<font color='red'>";
+        String postTag = "</font>";
+
+        Pageable  pageable = PageRequest.of(0, 10);
+        SortBuilder sortBuilder = new FieldSortBuilder("money")
+                .order(SortOrder.DESC);
+        SortBuilder ageBuilder = new FieldSortBuilder("age")
+                .order(SortOrder.DESC);
+
+
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.matchQuery("description", "bb man"))
+                .withHighlightFields(new HighlightBuilder.Field("description")
+                                    .preTags(preTag)
+                                    .postTags(postTag))
+                .withPageable(pageable)
+                .withSort(sortBuilder)
+                .withSort(ageBuilder)
+                .build();
+
+        AggregatedPage<Stu> stus = esTemplate.queryForPage(searchQuery, Stu.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse searchResponse, Class<T> aClass, Pageable pageable) {
+
+                List<Stu> stuList = new ArrayList<>();
+                SearchHits hits = searchResponse.getHits();
+
+                for (SearchHit hit : hits) {
+                    HighlightField description = hit.getHighlightFields().get("description");
+                    String desc = description.getFragments()[0].toString();
+
+                    Object stuId = (Object) hit.getSourceAsMap().get("stuId");
+                    String sign = (String) hit.getSourceAsMap().get("sign");
+                    String name = (String) hit.getSourceAsMap().get("name");
+                    Integer age = (Integer) hit.getSourceAsMap().get("age");
+                    Object money = (Object) hit.getSourceAsMap().get("money");
+                    Stu stu = new Stu();
+                    stu.setDescription(desc);
+                    stu.setSign(sign);
+                    stu.setName(name);
+                    stu.setStuId(Long.valueOf(stuId.toString()));
+                    stu.setAge(age);
+                    stu.setMoney(Integer.valueOf(money.toString()));
+                    stuList.add(stu);
+                }
+
+                if (null != stuList && stuList.size() > 0){
+                    return new AggregatedPageImpl<>((List<T>)stuList);
+
+                }
+                return null;
+            }
+        });
 
         System.out.println("总分页数目"+stus.getTotalPages());
         for (Stu stu : stus.getContent()) {
